6c82765b683425e3ed9774823a3d586675ab3999
hebaadel2001 <90560971+hebaadel2001@users.noreply.github.com>
2022-03-07 23:06:05 +0200

    Add files via upload

---
+++ DDA_Algorithm/DDA_Algorithm/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace DDA_Algorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boxForX0 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Y0 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.boxForY0 = new System.Windows.Forms.TextBox();
+            this.boxForX1 = new System.Windows.Forms.TextBox();
+            this.boxForY1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // boxForX0
+            // 
+            this.boxForX0.Location = new System.Drawing.Point(115, 121);
+            this.boxForX0.Name = "boxForX0";
+            this.boxForX0.Size = new System.Drawing.Size(125, 27);
+            this.boxForX0.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(433, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(656, 550);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Start Point ( X0 , Y0 ):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(34, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "X0";
+            // 
+            // Y0
+            // 
+            this.Y0.AutoSize = true;
+            this.Y0.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Y0.Location = new System.Drawing.Point(34, 177);
+            this.Y0.Name = "Y0";
+            this.Y0.Size = new System.Drawing.Size(33, 23);
+            this.Y0.TabIndex = 4;
+            this.Y0.Text = "Y0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(288, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Enter End Point ( X1 , Y1 ):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(34, 328);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 23);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "X1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(34, 392);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 23);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Y1";
+            // 
+            // boxForY0
+            // 
+            this.boxForY0.Location = new System.Drawing.Point(115, 177);
+            this.boxForY0.Name = "boxForY0";
+            this.boxForY0.Size = new System.Drawing.Size(125, 27);
+            this.boxForY0.TabIndex = 8;
+            // 
+            // boxForX1
+            // 
+            this.boxForX1.Location = new System.Drawing.Point(115, 328);
+            this.boxForX1.Name = "boxForX1";
+            this.boxForX1.Size = new System.Drawing.Size(125, 27);
+            this.boxForX1.TabIndex = 9;
+            // 
+            // boxForY1
+            // 
+            this.boxForY1.Location = new System.Drawing.Point(115, 391);
+            this.boxForY1.Name = "boxForY1";
+            this.boxForY1.Size = new System.Drawing.Size(125, 27);
+            this.boxForY1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(34, 469);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(342, 62);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Apply DDA Algorithm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1116, 574);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.boxForY1);
+            this.Controls.Add(this.boxForX1);
+            this.Controls.Add(this.boxForY0);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Y0);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.boxForX0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox boxForX0;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Y0;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox boxForY0;
+        private System.Windows.Forms.TextBox boxForX1;
+        private System.Windows.Forms.TextBox boxForY1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ DDA_Algorithm/DDA_Algorithm/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DDA_Algorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+                                          // When press on button:
+            private void button1_Click(object sender, EventArgs e)
+        {
+            /****************************************************************************
+                       get values of starting point (x , y) from its textBoxs: 
+            ****************************************************************************/
+            
+            int x0 = Convert.ToInt32(boxForX0.Text);  // => for X0
+            int y0 = Convert.ToInt32(boxForY0.Text);  // => for Y0
+            
+            /****************************************************************************
+                       get values of starting point (x , y) from its textBoxs: 
+            ****************************************************************************/
+
+            int x1 = Convert.ToInt32(boxForX1.Text);
+            int y1 = Convert.ToInt32(boxForY1.Text);
+            
+            /****************************************************************************
+                            add x0 , y0 in =>startPoint =>object from Point
+                            add x1 , y1 in =>endPoint =>object from Point
+             ***************************************************************************/
+            
+            Point startPoint = new Point(x0, y0);
+            Point endPoint = new Point(x1, y1);
+
+            /****************************************************************************
+                          Calling DDA Algorithm to apply it and draw the line
+             ***************************************************************************/
+
+            DDA_Algorithm(startPoint , endPoint);
+        
+        }
+
+                                                             // DDA algorithm => 
+        public void DDA_Algorithm(Point start, Point end)
+        {
+            /**********************************************************************************
+                          create a graghic object to draw in the panel
+                          abrush is the line that will be drawn
+             **********************************************************************************/
+            
+            var graphic = panel1.CreateGraphics();
+            var aBrush = Brushes.Black;
+
+            /*********************************************************************************
+                            calculate deltaX and deltaY: => deltaX = x1-x0
+                                                         => deltaY = y1-y0
+             ********************************************************************************/
+            
+            int dX = end.X - start.X;
+            int dY = end.Y - start.Y;
+            /********************************************************************************
+                            k is a variable that will be used in the for loop
+                            steps ia a variable to identify the number of steps to put pixel
+             ******************************************************************************/
+            int steps;
+            int k;
+
+            double incrementX;
+            double incrementY;
+            
+            /*******************************************************************************
+                                   assign the value of starting point to x and y
+             ******************************************************************************/
+            
+            float x = start.X;
+            float y = start.Y;
+            /****************************************************************************
+             in the other way:
+                    if (fabs (dx) > fabs (dy))
+                        steps = fabs (dx);
+                    else
+                        steps = fabs (dy);
+             *************************************************************************/
+            
+            steps = Math.Abs(dX) > Math.Abs(dY) ? Math.Abs(dX) : Math.Abs(dY);
+
+            /************************************************************************
+             Calculate xIncrement and yIncrement
+             ***********************************************************************/
+            
+            incrementX = dX / (float) steps;
+            incrementY = dY / (float) steps;
+            
+            /***********************************************************************
+             Math.Round => is a function returns the value of a number rounded to the nearest integer.
+             **********************************************************************/
+            
+            for (k = 0; k < steps; k++)
+            {
+                x = (float)(x + incrementX);
+                y = (float)(y + incrementY);
+                graphic.FillRectangle(aBrush, (int)Math.Round(x, 0), (int)Math.Round(y, 0), 2, 2);
+            }
+
+        }
+
+    }
+}
